2b717b0983549f4661f9fe96acc0d3e1ab4c05df
Reza <radhitya@uwaterloo.ca>
2021-05-16 18:02:28 -0400

    cleaning up

--- Reza_Raytracer/OBJReader.cpp
+++ Reza_Raytracer/OBJReader.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <sstream>
 
-std::vector<std::string> Split(const std::string& s,  char delim)
+inline std::vector<std::string> Split(const std::string& s,  char delim)
 {
 	std::vector<std::string> elems;
 
@@ -20,7 +20,8 @@ std::vector<std::string> Split(const std::string& s,  char delim)
 }
 
 
-inline bool StartWith(const std::string& prefix, const std::string& argument)
+inline bool StartWith(const std::string& prefix, 
+					  const std::string& argument)
 {
 	if (argument.substr(0, prefix.size()) == prefix)
 	{
@@ -29,7 +30,6 @@ inline bool StartWith(const std::string& prefix, const std::string& argument)
 	return false;
 }
 
-// github.com/azer89/MangaVectorizer/blob/master/CVSystem/OBJIO.cpp
 void OBJReader::ReadOBJ(const std::string& filename, 
 						std::vector<Vec3>& vertices,
 						std::vector< std::vector<int>>& faces)
@@ -40,27 +40,28 @@ void OBJReader::ReadOBJ(const std::string& filename,
 		std::string line;
 		std::getline(f, line);
 
-		if (line.size() == 0) { continue; }
-		std::vector<std::string> arrayStr = Split(line, ' ');
-
-		if (StartWith("v", line) && arrayStr.size() == 4)
-		{
-			vertices.push_back(Vec3(std::stod(arrayStr[1]), 
-									std::stod(arrayStr[2]), 
-									std::stod(arrayStr[3]) ));
+		if (line.size() == 0) 
+		{ 
+			continue; 
 		}
 
-		if (StartWith("f", line) && arrayStr.size() == 4)
+		std::vector<std::string> str_array = Split(line, ' ');
+
+		if (StartWith("v", line) && str_array.size() == 4)
 		{
-			int i1 = std::stoi( Split(arrayStr[1], '/')[0] );
-			int i2 = std::stoi( Split(arrayStr[2], '/')[0] );
-			int i3 = std::stoi( Split(arrayStr[3], '/')[0] );
+			vertices.push_back(Vec3(std::stod(str_array[1]),
+									std::stod(str_array[2]),
+									std::stod(str_array[3]) ));
+		}
+		else if (StartWith("f", line) && str_array.size() == 4)
+		{
+			int i1 = std::stoi( Split(str_array[1], '/')[0] );
+			int i2 = std::stoi( Split(str_array[2], '/')[0] );
+			int i3 = std::stoi( Split(str_array[3], '/')[0] );
 
-			faces.push_back({i1 - 1, i2 - 1, i3 - 1});
-
-			/*faces.push_back({ std::stoi(arrayStr[1]),
-							    std::stoi(arrayStr[2]),
-							    std::stoi(arrayStr[3]) });*/
+			faces.push_back({i1 - 1, 
+							 i2 - 1, 
+							 i3 - 1});
 		}
 	}
 
