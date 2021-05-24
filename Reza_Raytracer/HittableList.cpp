a343dd1482ee06eed842ee67d8026d319a2e9937
Reza <radhitya@uwaterloo.ca>
2021-05-24 18:05:20 -0400

    metal material is ok!

--- Reza_Raytracer/HittableList.cpp
+++ Reza_Raytracer/HittableList.cpp
@@ -45,13 +45,14 @@ void HittableList::CreateWorld()
     //auto material_light = make_shared<DiffuseLight>(Color(0.6, 0.7, 0.6));
     auto material_ground = make_shared<Lambertian>(Color(0.6, 0.7, 0.6));
     //auto material_center = make_shared<Lambertian>(Color(0.3, 0.3, 0.7));
-    auto material_center = make_shared<Metal>(Color(0.3, 0.3, 0.7));
+    auto material_side = make_shared<Metal>(Color(0.7, 0.3, 0.3));
+    auto material_center = make_shared<Lambertian>(Color(0.3, 0.3, 0.7));
 	
-    /*objects.push_back(make_shared<Sphere>(Point3(0, 0, -1),    0.5, material_center) );
-      objects.push_back(make_shared<Sphere>(Point3(-1.1, 0, -1), 0.5, material_center) );
-      objects.push_back(make_shared<Sphere>(Point3(1.1, 0, -1),  0.5, material_center) );
-    //objects.push_back(make_shared<Sphere>(Point3(0, 0, -1),    2.5, material_center) );*/
-    //objects.push_back(make_shared<Sphere>(Point3(0, -100, -1), 100, material_ground) );
+    objects.push_back(make_shared<Sphere>(Point3(0, 0, -1),    0.5, material_center) );
+    objects.push_back(make_shared<Sphere>(Point3(-1.1, 0, -1), 0.5, material_side) );
+    objects.push_back(make_shared<Sphere>(Point3(1.1, 0, -1),  0.5, material_side) );
+    //objects.push_back(make_shared<Sphere>(Point3(0, 0, -1),    2.5, material_center) );
+    objects.push_back(make_shared<Sphere>(Point3(0, -100.5, -1), 100, material_ground) );
     //objects.push_back(make_shared<Sphere>(Point3(0, 0, -1), 0.5, material_center));
         
     /*objects.push_back(make_shared<Triangle>(Point3( 0, 0.25,  0),
@@ -87,7 +88,7 @@ void HittableList::CreateWorld()
     */
 
     
-    obj_reader.ReadOBJ(filename, vertices, faces);
+    /*obj_reader.ReadOBJ(filename, vertices, faces);
     for (int i = 0; i < faces.size(); i++)
     {
         int i1 = faces[i][0];
@@ -102,7 +103,7 @@ void HittableList::CreateWorld()
                                                 p2,
                                                 p3,
                                                 material_center));
-    }
+    }*/
     
 
     // init BVH
