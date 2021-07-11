8e0c219dd53f15ce2e884a2ef10175dde69b27d8
Reza Saputra <radhitya@uwaterloo.ca>
2021-07-11 13:34:24 -0400

    Update Hittable.h

--- Reza_Raytracer/Hittable.h
+++ Reza_Raytracer/Hittable.h
@@ -14,23 +14,4 @@ public:
     virtual bool BoundingBox(AABB& output_box) const = 0;
 };
 
-class NullHittable : public Hittable
-{
-public:
-    NullHittable()
-    {
-    }
-
-    bool Hit(const Ray3& r, double t_min, double t_max, HitRecord& rec) const override
-    {
-        return false;
-    }
-
-    bool BoundingBox(AABB& output_box) const override
-    {
-        output_box = AABB();
-        return true;
-    }
-};
-
 #endif
