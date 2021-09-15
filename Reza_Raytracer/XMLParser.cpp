f1997157ac9526c64717df7fb10a0cf65252cd44
Reza Saputra <radhitya@uwaterloo.ca>
2021-09-15 10:41:13 -0400

    cornell box

--- Reza_Raytracer/XMLParser.cpp
+++ Reza_Raytracer/XMLParser.cpp
@@ -210,46 +210,57 @@ void AddCornellBox(std::unordered_map<std::string, shared_ptr<Texture>>& texture
     texture_map["cornell_green_tex"] = make_shared<SolidColorTexture>(Color(0.12, 0.45, 0.15)); // green
     texture_map["cornell_light_tex"] = make_shared<SolidColorTexture>(Color(15, 15, 15)); // light
 
-    mat_map["cornell_red_mat"]   = make_shared<DiffuseLightMaterial>(texture_map["cornell_red_tex"]); // red
-    mat_map["cornell_white_mat"] = make_shared<DiffuseLightMaterial>(texture_map["cornell_white_tex"]); // white
-    mat_map["cornell_green_mat"] = make_shared<DiffuseLightMaterial>(texture_map["cornell_green_tex"]); // green
+    mat_map["cornell_red_mat"]   = make_shared<LambertianMaterial>(texture_map["cornell_red_tex"]); // red
+    mat_map["cornell_white_mat"] = make_shared<LambertianMaterial>(texture_map["cornell_white_tex"]); // white
+    mat_map["cornell_green_mat"] = make_shared<LambertianMaterial>(texture_map["cornell_green_tex"]); // green
+
     mat_map["cornell_light_mat"] = make_shared<DiffuseLightMaterial>(texture_map["cornell_light_tex"]); // light
+
+    /*
+    <object type="yzrect" material_name="red_checker_material">
+      <boundary	y0="0" y1="5.55" z0="0" z1="5.55" x="5.55"/>
+    </object>
+    
+    <object type="yzrect" material_name="red_checker_material">
+      <boundary	y0="0" y1="5.55" z0="0" z1="5.55" x="0"/>
+    </object>
+    */
 
     //                                       y0   y1    z0    z1    x
     objects.emplace_back(make_shared<YZRect>(0,   5.55, 0,    5.55, 5.55, mat_map["cornell_green_mat"]));
     objects.emplace_back(make_shared<YZRect>(0,   5.55, 0,    5.55, 0,    mat_map["cornell_red_mat"]));
+    
     /*
-    <object type="yzrect" material_name="material_checker">
-      <boundary	y0="0" y1="5.55" z0="0" z1="5.55" x="5.55"/>
-    </object>
-    <object type="yzrect" material_name="material_checker">
-      <boundary	y0="0" y1="5.55" z0="0" z1="5.55" x="0"/>
+    <object type="xzrect" material_name="material_light">
+      <boundary	x0="1.13" x1="4.43" z0="1.27" z1="4.32" y="5.54"/>
     </object>
     */
-    
+
     //                                       x0   x1    z0    z1    y
-    objects.emplace_back(make_shared<XZRect>(2.13, 3.43, 2.27, 3.32, 5.54, mat_map["cornell_light_mat"]));
+    objects.emplace_back(make_shared<XZRect>(1.13, 4.43, 1.27, 4.32, 5.54, mat_map["cornell_light_mat"]));
+
+
+    /*
+    <object type="xzrect" material_name="blue_checker_material">
+      <boundary	x0="0" x1="5.55" z0="0" z1="5.55" y="0"/>
+    </object>
+    
+    <object type="xzrect" material_name="blue_checker_material">
+      <boundary	x0="0" x1="5.55" z0="0" z1="5.55" y="5.55"/>
+    </object>
+    */
+
     objects.emplace_back(make_shared<XZRect>(0,   5.55, 0,    5.55, 0,    mat_map["cornell_white_mat"]));
     objects.emplace_back(make_shared<XZRect>(0,   5.55, 0,    5.55, 5.55, mat_map["cornell_white_mat"]));
+
     /*
-    <object type="xzrect" material_name="material_checker">
-		<boundary	x0="2.13" x1="3.43" z0="2.27" z1="3.32" y="5.54"/>
-	</object>
-    <object type="xzrect" material_name="material_checker">
-        <boundary	x0="0" x1="5.55" z0="0" z1="5.55" y="0"/>
-    </object>
-    <object type="xzrect" material_name="material_checker">
-        <boundary	x0="0" x1="5.55" z0="0" z1="5.55" y="5.55"/>
-    </object>
+    <object type="xyrect" material_name="green_checker_material">
+      <boundary	x0="0" x1="5.55" y0="0" y1="5.55" z="0"/>
+    </object-->
     */
 
     //                                       x0   x1    y0    y1    z
     objects.emplace_back(make_shared<XYRect>(0,   5.55, 0,    5.55, 0, mat_map["cornell_white_mat"]));
-    /*
-    <object type="xyrect" material_name="material_checker">
-      <boundary	x0="0" x1="5.55" y0="0" y1="5.55" z="5.55"/>
-    </object>
-    */
 }
 
 void AddTriangleMesh(XMLElement* elem,
