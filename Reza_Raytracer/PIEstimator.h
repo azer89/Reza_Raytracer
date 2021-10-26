d1547d51193fe0cac65840620a2c80b5c3f514fc
Reza Saputra <radhitya@uwaterloo.ca>
2021-10-26 17:48:10 -0400

    Update PIEstimator.h

--- Reza_Raytracer/PIEstimator.h
+++ Reza_Raytracer/PIEstimator.h
@@ -1,6 +1,8 @@
 #ifndef __PI_ESTIMATOR_H__
 #define __PI_ESTIMATOR_H__
 
+#include "UsefulThings.h"
+
 class PIEstimator
 {
 	static void Calculate()
@@ -8,6 +10,25 @@ class PIEstimator
 		int inside_circle = 0;
 		int inside_circle_stratified = 0;
 		int sqrt_N = 10000;
+
+        for (int i = 0; i < sqrt_N; i++) 
+        {
+            for (int j = 0; j < sqrt_N; j++) 
+            {
+                auto x = UsefulFunct::RandomDouble(-1, 1);
+                auto y = UsefulFunct::RandomDouble(-1, 1);
+                if (x * x + y * y < 1)
+                {
+                    inside_circle++;
+                }
+                x = 2 * ((i + UsefulFunct::RandomDouble()) / sqrt_N) - 1;
+                y = 2 * ((j + UsefulFunct::RandomDouble()) / sqrt_N) - 1;
+                if (x * x + y * y < 1)
+                {
+                    inside_circle_stratified++;
+                }
+            }
+        }
 	}
 };
 
