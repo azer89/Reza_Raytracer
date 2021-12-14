5181792d042062a4e8ae11a556ca8e2b063c2a3b
Reza Saputra <radhitya@uwaterloo.ca>
2021-12-14 17:06:33 -0500

    Update Sphere.h

--- Reza_Raytracer/Sphere.h
+++ Reza_Raytracer/Sphere.h
@@ -8,19 +8,19 @@ class Sphere : public Hittable
 {
 public:
     // Constructor 
-	Sphere(Point3 cen, double r, shared_ptr<Material>& m)
+    Sphere(Point3 cen, double r, shared_ptr<Material>& m)
     {
         center = cen;
         radius = r;
-        
+
         // Copy shared_ptr 
         material_ptr = m;
     }
 
-    bool Hit(const Ray3& r, 
-			 double t_min, 
-			 double t_max, 
-			 HitRecord& rec) const override;
+    bool Hit(const Ray3& r,
+        double t_min,
+        double t_max,
+        HitRecord& rec) const override;
 
     bool BoundingBox(AABB& output_box) const override;
 
@@ -28,7 +28,7 @@ private:
     // TODO refactor to return a Vec2
     static void GetSphereUV(const Point3& p, double& u, double& v);
 
-public:    
+public:
     double radius;
     Point3 center;
     shared_ptr<Material> material_ptr;
