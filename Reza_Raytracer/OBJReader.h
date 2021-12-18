80112cff8d9babd8ac0aa9835f8ace5fea1e5800
Reza Saputra <radhitya@uwaterloo.ca>
2021-12-18 14:50:10 -0500

    Update OBJReader.h

--- Reza_Raytracer/OBJReader.h
+++ Reza_Raytracer/OBJReader.h
@@ -15,17 +15,17 @@ public:
 
 	// Supports "v" and "f" 
 	void ReadOBJ(const std::string& filename,
-				 std::vector<Vec3>& vertices,
-				 std::vector< std::vector<int>>& faces);
+		std::vector<Vec3>& vertices,
+		std::vector< std::vector<int>>& faces);
 
 	// Supports "v", "vn", "vt", and "f"
 	void ReadOBJ(const std::string& filename,
-				 std::vector<Vec3>& vertices,
-				 std::vector<Vec3>& normals,
-				 std::vector<Vec2>& uvs,
-				 std::vector<std::vector<int>>& vertex_indices,
-				 std::vector<std::vector<int>>& normal_indices,
-				 std::vector<std::vector<int>>& uv_indices);
+		std::vector<Vec3>& vertices,
+		std::vector<Vec3>& normals,
+		std::vector<Vec2>& uvs,
+		std::vector<std::vector<int>>& vertex_indices,
+		std::vector<std::vector<int>>& normal_indices,
+		std::vector<std::vector<int>>& uv_indices);
 };
 
 #endif
