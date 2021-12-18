6d289f75224fe435cfc24e0e7287eab929bb8ec9
Reza Saputra <radhitya@uwaterloo.ca>
2021-12-17 20:27:57 -0500

    Update Ray3.h

--- Reza_Raytracer/Ray3.h
+++ Reza_Raytracer/Ray3.h
@@ -8,32 +8,32 @@ class Ray3
 public:
     Point3 origin;
     Vec3 direction;
-	
+
 public:
-	// Constructor
+    // Constructor
     Ray3()
-    {	    
+    {
     }
 
     // Constructor
     Ray3(const Point3& orig, const Vec3& dir)
         : origin(orig),
-		  direction(dir)
+        direction(dir)
     {}
 
-    Point3 Origin() const 
-    { 
-        return origin; 
+    Point3 Origin() const
+    {
+        return origin;
     }
-	
-    Vec3 Direction() const 
-    { 
-        return direction; 
+
+    Vec3 Direction() const
+    {
+        return direction;
     }
 
     // t = [0, 1]
     Point3 At(double t) const
-	{
+    {
         return origin + t * direction;
     }
 };
