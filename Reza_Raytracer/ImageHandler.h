c9c4c38119574386a5163bad7b0796bb165b10fa
Reza Saputra <radhitya@uwaterloo.ca>
2021-12-20 18:08:31 -0500

    Update ImageHandler.h

--- Reza_Raytracer/ImageHandler.h
+++ Reza_Raytracer/ImageHandler.h
@@ -9,15 +9,15 @@ class ImageHandler
 {
 private:
 	std::unique_ptr<uint8_t[]> pixels;
-	
+
 	int img_width;
 	int img_height;
 	int num_channel;
-	
+
 public:
 	// Constructor
 	ImageHandler(int img_width, int img_height);
-	
+
 	// Destructor
 	~ImageHandler();
 
