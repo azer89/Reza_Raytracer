a3c14b87d0b0ec7cd849f08508e0cd49d1310237
Reza Saputra <radhitya@uwaterloo.ca>
2021-12-21 17:00:32 -0500

    Update Hittable.h

--- Reza_Raytracer/Hittable.h
+++ Reza_Raytracer/Hittable.h
@@ -13,7 +13,7 @@ public:
     // More about ray interections:
     // www.iquilezles.org/www/articles/intersectors/intersectors.htm
     virtual bool Hit(const Ray3& r, double t_min, double t_max, HitRecord& rec) const = 0;
-    
+
     virtual bool BoundingBox(AABB& output_box) const = 0;
 };
 
