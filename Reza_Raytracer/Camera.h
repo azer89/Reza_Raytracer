6691e8c7740e2b07e50ee6626ccb340e06e56ed6
Reza Saputra <radhitya@uwaterloo.ca>
2021-12-24 17:17:49 -0500

    Update Camera.h

--- Reza_Raytracer/Camera.h
+++ Reza_Raytracer/Camera.h
@@ -8,22 +8,22 @@ class Camera
 {
 public:
     Camera();
-	
+
     Ray3 GetRay(double s, double t) const;
 
-	double GetAspectRatio() const
-	{
+    double GetAspectRatio() const
+    {
         return aspect_ratio;
-	}
+    }
 
-private:   
+private:
     double vfov; // Vertical field-of-view in degrees
     double aspect_ratio;
 
     double viewport_height;
     double viewport_width;
     //double focal_length;
-	
+
     Point3 origin;
     Point3 lower_left_corner;
     Vec3 horizontal;
