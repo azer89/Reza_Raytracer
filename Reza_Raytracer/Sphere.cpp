8c15b9f0dc990a7cf68637b6c23adea99fb1383b
Reza Saputra <radhitya@uwaterloo.ca>
2022-01-12 19:08:34 -0500

    Update Sphere.cpp

--- Reza_Raytracer/Sphere.cpp
+++ Reza_Raytracer/Sphere.cpp
@@ -74,15 +74,6 @@ bool Sphere::BoundingBox(AABB& output_box) const
 
 void Sphere::GetSphereUV(const Point3& p, double& u, double& v)
 {
-    /*
-    p: a given point on the sphere of radius one, centered at the origin.
-    u: returned value [0,1] of angle around the Y axis from X=-1.
-    v: returned value [0,1] of angle from Y=-1 to Y=+1.
-        <1 0 0> yields <0.50 0.50>      <-1  0  0> yields <0.00 0.50>
-        <0 1 0> yields <0.50 1.00>      < 0 -1  0> yields <0.50 0.00>
-        <0 0 1> yields <0.25 0.50>      < 0  0 -1> yields <0.75 0.50>
-    */
-
     auto theta = acos(-p.y());
     auto phi = atan2(-p.z(), p.x()) + UsefulConst::pi;
 
